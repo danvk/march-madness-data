4b367cbe71b6b3ba911de5db5630e75b22a4a725
Dan Vanderkam <danvdk@gmail.com>
2018-03-17 15:21:35 -0400

    Complete analysis

--- extract_bracket.py
+++ extract_bracket.py
@@ -14,17 +14,42 @@ def dict_to_array(d):
     return [d[k] for k in sorted(d.keys())]
 
 
+DEFAULTS16 = [
+    ('RD1-seed01', '1'),
+    ('RD1-seed02', '16'),
+    ('RD1-seed03', '8'),
+    ('RD1-seed04', '9'),
+    ('RD1-seed05', '5'),
+    ('RD1-seed06', '12'),
+    ('RD1-seed07', '4'),
+    ('RD1-seed08', '13'),
+    ('RD1-seed09', '6'),
+    ('RD1-seed10', '11'),
+    ('RD1-seed11', '3'),
+    ('RD1-seed12', '14'),
+    ('RD1-seed13', '7'),
+    ('RD1-seed14', '10'),
+    ('RD1-seed15', '2'),
+    ('RD1-seed16', '15')
+]
+
+
 def extract_template(template):
     # round # --> game # --> 0 or 1 --> {team|score|seed} --> value
     rounds = defaultdict(lambda: defaultdict(lambda: [{}, {}]))
-    for p in template.params:
-        m = re.match(NAME_RE, str(p.name))
+    pairs = [
+        (str(p.name), p.value.strip_code().strip())
+        for p in template.params
+    ]
+    if template.name.matches('16TeamBracket'):
+        pairs += DEFAULTS16
+    for name, value in pairs:
+        m = re.match(NAME_RE, name)
         if not m:
             continue
         round_num = int(m.group(1)) - 1
         field = m.group(2)
         slot = int(m.group(3)) - 1
-        value = p.value.strip_code().strip()
 
         game_num = slot // 2
         game_slot = slot % 2
@@ -36,7 +61,11 @@ def extract_template(template):
 def extract_bracket(source):
     wikicode = mwparserfromhell.parse(source)
     templates = wikicode.filter_templates()
-    brackets = [t for t in templates if 'Bracket' in t.name]
+    brackets = [
+        t
+        for t in templates
+        if 'Bracket' in t.name and not t.name.matches('2TeamBracket')
+    ]
     return [
         extract_template(b)
         for b in brackets
--- find_highest_seeds.py
+++ find_highest_seeds.py
@@ -26,17 +26,37 @@ def extract_seed(seed):
 
 
 def sum_for_game(game):
-    print(game)
     return extract_seed(game[0]['seed']) + extract_seed(game[1]['seed'])
 
 
+def extract_year(path):
+    m = re.search(r'(\d\d\d\d)', path)
+    assert m, path
+    return int(m.group(1))
+
+
+def format_result(game_path):
+    game, path = game_path
+    return '(%s) %2d: %20s %2d vs %2d %-20s' % (
+        extract_year(path),
+        sum_for_game(game),
+        game[0]['team'],
+        extract_seed(game[0]['seed']),
+        extract_seed(game[1]['seed']),
+        game[1]['team'],
+    )
+
+
 def main():
-    games = []
+    game_paths = []
     for path in sys.argv[1:]:
         bracket = json.load(open(path))
-        games += [(game, path) for game in all_games(bracket)]
-    ordered_games = sorted(games, key=lambda gp: -sum_for_game(gp[0]))
-    print(ordered_games[:10])
+        for game in all_games(bracket):
+            sum_for_game(game)
+        game_paths += [(game, path) for game in all_games(bracket)]
+    ordered_games = sorted(game_paths, key=lambda gp: -sum_for_game(gp[0]))
+    for game_path in ordered_games[:30]:
+        print(format_result(game_path))
 
 
 if __name__ == '__main__':
